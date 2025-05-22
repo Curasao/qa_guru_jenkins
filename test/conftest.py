fbff0ebf26be27099c466db3dd538f66645060e4
Curasao <rudarya4@gmail.com>
2025-05-23 02:01:04 +0300

    Homework

--- test/conftest.py
+++ test/conftest.py
@@ -14,8 +14,8 @@ def setup_browser(request):
     browser.config.driver_options = driver_options
     browser.config.base_url = 'https://demoqa.com'
 
-    #browser.config.window_width = 1920
-    #browser.config.window_height = 1080
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     print("BO SINN")
     options = Options()
     selenoid_capabilities = {
